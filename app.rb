bde1a5ef5fc60cba137ec0deb986ad0442f9fb1d
Ty Cook <tycook@Tys-MacBook-Pro.local>
2017-09-22 14:19:30 -0400

    added a little bit of login, but need to finish search function first

--- app.rb
+++ app.rb
@@ -14,6 +14,22 @@ db_params = {
 db = PG::Connection.new(db_params)
 
 get '/' do
+    login_info = db.exec("Select * From phonebook_username_password_login_table")
+    erb :login, locals: {login_info: login_info}
+end
+
+post '/login' do
+    new_username = params[:sign_up_username]
+    new_password = params[:sign_up_password]
+    checked_username = params[:user_given_username]
+    checked_password = params[:user_given_password]
+    
+    db.exec("INSERT INTO public.phonebook_username_password_login_table(username_column, password_column) VALUES('#{new_username}', '#{new_password}')");
+
+    redirect '/'
+end
+
+get '/index' do
     phonebook = db.exec("Select * From user_given_data_table")
     erb :index, locals: {phonebook: phonebook}
 end
@@ -30,7 +46,7 @@ post '/index' do
 
     db.exec("INSERT INTO public.user_given_data_table(first_name, last_name, street_name, city, state, zip, phone_number, email) VALUES('#{fname}', '#{lname}', '#{street}', '#{city}', '#{state}', '#{zip}', '#{phone}', '#{email}')");
 
-    redirect '/'
+    redirect '/index'
 end
 
 post '/updated_table_cell' do
@@ -56,7 +72,7 @@ post '/updated_table_cell' do
         when 'new_email'
             db.exec("UPDATE user_given_data_table SET email = '#{new_entry}' WHERE email = '#{old_entry}'");
     end
-    redirect '/'
+    redirect '/index'
 end
 
 post '/delete_table_row' do
@@ -81,5 +97,5 @@ post '/delete_table_row' do
         when 'delete_email'
             db.exec("DELETE FROM user_given_data_table WHERE email = '#{deleted}'");
     end
-    redirect '/'
+    redirect '/index'
 end
